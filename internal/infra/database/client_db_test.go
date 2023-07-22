f43d16e42c7ecb8119946fa40757d6ddbbcc8739
emannuel <emannuelloxt@hotmail.com>
2023-07-22 18:36:30 -0400

    test: create clients test suite

---
+++ internal/infra/database/client_db_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ClientDBTestSuite struct {
+	suite.Suite
+	db       *sql.DB
+	clientDB *ClientDB
+}
+
+func (s *ClientDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+
+	s.db = db
+	db.Exec("CREATE TABLE clients (id varchar(255) primary key, name varchar(255), email varchar(255), created_at timestamp)")
+	s.clientDB = NewClientDB(db)
+}
+
+func (s *ClientDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDBTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDBTestSuite))
+}
